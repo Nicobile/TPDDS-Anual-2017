58334522f461c9b2971c2de11c357c718c82a56a
nicobile <nico_nobile@hotmail.com>
2017-04-14 19:51:36 -0300

    Agregadas pruebas

--- src/test/java/tp/pruebaArchivo.java
+++ src/test/java/tp/pruebaArchivo.java
@@ -46,9 +46,25 @@ public class pruebaArchivo {
 	
 	public void verficarCuentasEmpresa() throws FileNotFoundException, IOException{
 		assertEquals(this.lector.getEmpresas().get(2).getCuentas().get(0).getNombre(),"cuentaB");
+		assertEquals(this.lector.getEmpresas().get(2).getCuentas().get(0).getFecha(),"11/02/2011");
+		assertEquals(this.lector.getEmpresas().get(2).getCuentas().get(0).getValor(),654263);
+		assertEquals(this.lector.getEmpresas().get(2).getCuentas().get(1).getNombre(),"cuentaC");
+		assertEquals(this.lector.getEmpresas().get(2).getCuentas().get(1).getFecha(),"11/02/2011");
+		assertEquals(this.lector.getEmpresas().get(2).getCuentas().get(1).getValor(),653);
+
 	}
-
-
+	
+	@Test
+	
+	public void verificarCantidadCuentasEmpresa()throws FileNotFoundException, IOException{
+		assertEquals(this.lector.getEmpresas().get(0).getCuentas().size(),1);
+		assertEquals(this.lector.getEmpresas().get(2).getCuentas().size(),2);
+		assertEquals(this.lector.getEmpresas().get(1).getCuentas().size(),1);
+		assertEquals(this.lector.getEmpresas().get(3).getCuentas().size(),2);
+		
+	}
+	
+	
     @After
     
     public void eliminarLista(){
