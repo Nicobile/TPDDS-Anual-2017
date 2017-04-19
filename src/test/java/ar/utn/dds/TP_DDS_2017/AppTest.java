11be62258b6077c538e7b6b2568a7b96ac9e0eac
gnfigueroa <gabrieln.figueroa@yahoo.com>
2017-04-18 22:52:03 -0300

    Cambios en Test.java

--- src/test/java/ar/utn/dds/TP_DDS_2017/AppTest.java
+++ src/test/java/ar/utn/dds/TP_DDS_2017/AppTest.java
@@ -11,9 +11,9 @@ public class AppTest
     extends TestCase
 {
     /**
-     * Create the test case
+     * Create the test case.
      *
-     * @param testName name of the test case
+     * @param testName name of the test case.
      */
     public AppTest( String testName )
     {
@@ -21,15 +21,16 @@ public class AppTest
     }
 
     /**
-     * @return the suite of tests being tested
+     * @return the suite of tests being tested.
      */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
+
     /**
-     * Rigourous Test :-)
+     * Rigourous Test :-).
      */
     public void testApp()
     {
