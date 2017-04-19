b58054af143d75cfd54ae03480454dc6fa076fcb
gnfigueroa <gabrieln.figueroa@yahoo.com.ar>
2017-04-18 22:42:05 -0300

    cambios en Cuenta.java

--- src/main/java/tp/Cuenta.java
+++ src/main/java/tp/Cuenta.java
@@ -20,18 +20,23 @@ public class Cuenta {
 public String getFecha() {
 	return fecha;
 }
+
 public void setFecha(String fecha) {
 	this.fecha = fecha;
 }
+
 public String getNombre() {
 	return nombre;
 }
+
 public void setNombre(String nombre) {
 	this.nombre = nombre;
 }
+
 public int getValor() {
 	return valor;
 }
+
 public void setValor(int valor) {
 	this.valor = valor;
 }
