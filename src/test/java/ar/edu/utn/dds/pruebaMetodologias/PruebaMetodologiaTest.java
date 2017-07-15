765dfb01f10a27af28a1e31dffcab8600ce7428d
nicobile <nico_nobile@hotmail.com>
2017-07-15 01:26:36 -0300

    Agrego mas test, hay uno que rompe

--- src/test/java/ar/edu/utn/dds/pruebaMetodologias/PruebaMetodologiaTest.java
+++ src/test/java/ar/edu/utn/dds/pruebaMetodologias/PruebaMetodologiaTest.java
@@ -18,7 +18,9 @@ import ar.edu.utn.dds.excepciones.NoSeEncuentraLaCuentaEnEsaFecha;
 import ar.edu.utn.dds.excepciones.NoSeEncuentraLaEmpresa;
 import ar.edu.utn.dds.excepciones.NoSePudoOrdenarLaCondicion;
 import ar.edu.utn.dds.modelo.Condicion;
+import ar.edu.utn.dds.modelo.Creciente;
 import ar.edu.utn.dds.modelo.Decreciente;
+import ar.edu.utn.dds.modelo.Longevidad;
 import ar.edu.utn.dds.modelo.Metodologia;
 import ar.edu.utn.dds.modelo.Promedio;
 import ar.edu.utn.dds.modelo.PuntajeEmpresa;
@@ -54,7 +56,9 @@ public class PruebaMetodologiaTest {
 		ArrayList<PuntajeEmpresa> empresas = meto.aplicarMetodologia();
 		//Solo Twitter, Cocacola y Facebook cumplen con la condidion de que su deuda sea decreciente
 		assertEquals(empresas.size(),3);
-		
+		assertEquals(empresas.get(0).getNombreEmpresa(),"Facebook");
+		assertEquals(empresas.get(1).getNombreEmpresa(),"CocaCola");
+		assertEquals(empresas.get(2).getNombreEmpresa(),"Twitter");
 	}
 	
 	
@@ -72,9 +76,56 @@ public class PruebaMetodologiaTest {
 
 	}
 	
+	@Test
+	public void pruebaSoloCondicionLongevidad() throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
+		Longevidad lon = new Longevidad(t.buscarIndicador("i_IngresoNeto"),t);
+		Condicion condlon = new Condicion(lon,3.0,">",3);	
+		meto.agregarCondicion(condlon);
+		ArrayList<PuntajeEmpresa> empresas = meto.aplicarMetodologia();
+		/*Todas las empresas tienen mas de 3 años"*/
+		assertEquals(empresas.size(),4);
+	}
+	
+	@Test
+	public void pruebaSoloCondicionCreciente()throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
+		Creciente cre = new Creciente(t.buscarIndicador("i_NivelDeuda"),t);
+		Condicion concre = new Condicion(cre,2,"mayor");
+		meto.agregarCondicion(concre);
+		ArrayList<PuntajeEmpresa> empresas = meto.aplicarMetodologia();
+		/*PEPSI incremento su deuda en 123,69 */
+		//assertEquals(empresas.get(0).getNombreEmpresa(),"Pepsico");
+		for(int i = 0; i < empresas.size(); i++){
+			System.out.println(empresas.get(i).getNombreEmpresa());
+			System.out.println(empresas.get(i).getPuntaje());
+		}
+		
+	}
+	
+	@Test
+	public void pruebaSoloPromedio()throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
+		Promedio pro = new Promedio(t.buscarIndicador("i_NivelDeuda"),t);
+		Condicion conpro = new Condicion (pro,200.0,">",2);
+		meto.agregarCondicion(conpro);
+		ArrayList<PuntajeEmpresa> empresas = meto.aplicarMetodologia();
+		/*Solo facebook tiene un promedio de deuda mayor a 200. Su promedio es de 282*/
+		assertEquals(empresas.size(),1);
+		assertEquals(empresas.get(0).getNombreEmpresa(),"Facebook");
+	}
 	
 	
-	@Test   //EL TEST QUE ROMPE TODO CON DE TODO
+	@Test
+	public void pruebaLongevidadyCreciente()throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
+		Longevidad lon = new Longevidad(t.buscarIndicador("i_IngresoNeto"),t);
+		Creciente cre = new Creciente(t.buscarIndicador("i_NivelDeuda"),t);
+		Condicion condlon = new Condicion(lon,3.0,">",3);
+		Condicion condcre = new Condicion(cre,2,"mayor");
+		meto.agregarCondicion(condlon);
+		meto.agregarCondicion(condcre);
+		ArrayList<PuntajeEmpresa> empresas = meto.aplicarMetodologia();
+		//assertEquals(empresas.get(1).getNombreEmpresa(),"Pepsico");
+	}
+
+	@Test   
 	public void pruebaSumtoriayDecreciente() throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
 		Decreciente decre= new Decreciente(t.buscarIndicador("i_NivelDeuda"),t);
 		Sumatoria sum=new Sumatoria(t.buscarIndicador("i_NivelDeuda"),t);
@@ -89,8 +140,8 @@ public class PruebaMetodologiaTest {
 	
 	
 	
-	@Test    //EL DE MAL ORDER?
-	public void pruebaPromedioySumatoria() throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
+	@Test   
+	public void pruebaPromedioySumatoriaMayoraMenor() throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
 		Promedio prom = new Promedio(t.buscarIndicador("i_NivelDeuda"),t);
 		Sumatoria sum=new Sumatoria(t.buscarIndicador("i_NivelDeuda"),t);
 		Condicion cond1 = new Condicion(prom,2,"mayor");
@@ -101,12 +152,21 @@ public class PruebaMetodologiaTest {
 		//El que tiene mayor promedio del indicador de deuda es Facebook
 		assertEquals(empresas.get(0).getNombreEmpresa(),"Facebook");
 		assertEquals(empresas.get(1).getNombreEmpresa(),"Pepsico");
-		for(int i=0;i<empresas.size();i++){
-			System.out.println(empresas.get(i).getNombreEmpresa());
-			System.out.println(empresas.get(i).getPuntaje());
-		}
-		
 		//Esas dos empresas son las unicas cuya sumatoria de Nivel de Deuda sea mayor que 7, en el periodo de 2 años
+	}
+	
+	@Test   
+	public void pruebaPromedioySumatoriaMenoraMayor() throws NoSeEncuentraElIndicador, NoSeEncuentraLaEmpresa, ScriptException, NoSePudoOrdenarLaCondicion, NoSeEncuentraLaCuenta, NoSeEncuentraLaCuentaEnEsaFecha{
+		Promedio prom = new Promedio(t.buscarIndicador("i_NivelDeuda"),t);
+		Sumatoria sum=new Sumatoria(t.buscarIndicador("i_NivelDeuda"),t);
+		Condicion cond1 = new Condicion(prom,2,"menor");
+		Condicion cond2 = new Condicion(sum,7.0,">",2);		
+		meto.agregarCondicion(cond1);
+		meto.agregarCondicion(cond2);
+		ArrayList<PuntajeEmpresa> empresas = meto.aplicarMetodologia();
+		//El que tiene menor promedio del indicador de deuda es Pepsico
+		assertEquals(empresas.get(0).getNombreEmpresa(),"Pepsico");
+		assertEquals(empresas.get(1).getNombreEmpresa(),"Facebook");
 	} 
 	
 	@After
