28bb5ff0951559e243d252589201aacbbc1d56b2
lucasAmoroso <lucas.-94@outlook.com>
2017-08-13 12:07:31 -0300

    saco un sysout

--- src/main/java/ar/edu/utn/dds/interfazGrafica/TiposDeCondicion.java
+++ src/main/java/ar/edu/utn/dds/interfazGrafica/TiposDeCondicion.java
@@ -26,7 +26,7 @@ public class TiposDeCondicion implements Initializable {
 
 	public void setT(Traductor tr) {
 		this.t = tr;
-		System.out.println(t.getIndicadores().size());
+		
 	}
 
 	public void setStagePrincipal(Stage stagePrincipal) {
