5c4ba1cafe0a00944f7db51c0510fd3f91b0064e
gnfigueroa <gabrieln.figueroa@yahoo.com>
2017-09-15 23:16:56 -0300

    cargando test de metodologia

--- src/test/java/ar/edu/utn/dds/pruebaPersistencia/MetodologiaTest.java
+++ src/test/java/ar/edu/utn/dds/pruebaPersistencia/MetodologiaTest.java
@@ -7,12 +7,14 @@ import java.io.IOException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
 import javax.script.ScriptException;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import ar.edu.utn.dds.entidades.Metodologias;
+import ar.edu.utn.dds.excepciones.MetodologiaYaExisteException;
 import ar.edu.utn.dds.excepciones.NoHayEmpresasQueCumplanLaCondicionException;
 import ar.edu.utn.dds.excepciones.NoSeEncuentraElIndicadorException;
 import ar.edu.utn.dds.excepciones.NoSeEncuentraLaCuentaEnElPeriodoException;
@@ -32,21 +34,41 @@ import ar.edu.utn.dds.procesarArchivos.ProcesarIndicadores;
 public class MetodologiaTest {
 	private ProcesarIndicadores procesador1;
 	private Traductor t;
-	private Metodologia meto;
+	private Metodologia metod;
 	private LectorArchivo lector;
 	@Before
 	public void inicializacion() throws FileNotFoundException, IOException, NoSeEncuentraLaEmpresaException {
 		this.t = new Traductor();
-		this.meto = new Metodologia("Metod");
+		this.metod = new Metodologia("Metod");
 		this.procesador1 = new ProcesarIndicadores(t);
-		this.lector = new LectorArchivo(t);
-		this.lector.leerArchivo(this.getClass().getResource("/Datos.txt").getFile());
+		//this.lector = new LectorArchivo(t);
+		//this.lector.leerArchivo(this.getClass().getResource("/Datos.txt").getFile());
 
 	}
-	//@Test
-	public void aplicarMetodologiaDesdeBD() throws NoSeEncuentraElIndicadorException, FileNotFoundException, IOException,
-			NoSeEncuentraLaEmpresaException, NoHayEmpresasQueCumplanLaCondicionException, ScriptException, NoSePudoOrdenarLaCondicionException, NoSeEncuentraLaCuentaException, NoSeEncuentraLaCuentaEnElPeriodoException {
+	@Test
+	//se ingresa en la base de datos la metodologia y se busca la misma metodologia.
+	public void crearMetodologiaDesdeBD(){
 		
+		t.agregarMetodologia(metod);
+		
+		//realizo la persistencia del objeto metodologia
+		Utilidades.persistirUnObjeto(metod);
+		
+		Metodologia m = t.buscarMetodologia("Metod");
+		assertEquals(metod,m);
+	}
 	
+	@Test(expected=PersistenceException.class)
+	//se evalua ingresar el mismo nombre de metodologia 
+	//sale error "Ya existe una metodologia con ese nombre"
+	public void crearMismaMetodologiaDesdeBD(){
+		
+		metod = new Metodologia("Metod");
+		
+		t.agregarMetodologia(metod);
+		
+		//realizo la persistencia del objeto metodologia
+		Utilidades.persistirUnObjeto(metod);
+
 	}
 }
