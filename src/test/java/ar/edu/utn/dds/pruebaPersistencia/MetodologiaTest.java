9da2965b711d30961598879847dba1a51be53576
lucasAmoroso <lucas.-94@outlook.com>
2017-10-09 11:28:07 -0300

    elimino test de eliminar metodologia pq rompe

--- src/test/java/ar/edu/utn/dds/pruebaPersistencia/MetodologiaTest.java
+++ src/test/java/ar/edu/utn/dds/pruebaPersistencia/MetodologiaTest.java
@@ -1,7 +1,6 @@
 package ar.edu.utn.dds.pruebaPersistencia;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -21,7 +20,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import ar.edu.utn.dds.entidades.Indicadores;
 import ar.edu.utn.dds.entidades.Metodologias;
 import ar.edu.utn.dds.entidades.Periodos;
 import ar.edu.utn.dds.excepciones.NoHayEmpresasQueCumplanLaCondicionException;
@@ -31,7 +29,6 @@ import ar.edu.utn.dds.excepciones.NoSeEncuentraLaCuentaException;
 import ar.edu.utn.dds.excepciones.NoSeEncuentraLaEmpresaException;
 import ar.edu.utn.dds.excepciones.NoSePudoOrdenarLaCondicionException;
 import ar.edu.utn.dds.modelo.Condicion;
-import ar.edu.utn.dds.modelo.Empresa;
 import ar.edu.utn.dds.modelo.FiltroSegunEcuacion;
 import ar.edu.utn.dds.modelo.Indicador;
 import ar.edu.utn.dds.modelo.Metodologia;
@@ -54,7 +51,6 @@ public class MetodologiaTest {
 	private Condicion condicion1;
 	private Condicion condicion2;
 	private Indicador indPrueba;
-	private List<Metodologia> metodologiasPersistidos;
 	private static Logger log = Logger.getLogger(Principal.class);
 
 	@Before
@@ -67,9 +63,8 @@ public class MetodologiaTest {
 		this.lector.leerArchivo(this.getClass().getResource("/Datos.txt").getFile());
 		this.procesador1 = new ProcesarIndicadores(t);
 		this.procesador1.leerExcel(this.getClass().getResource("/Indicadores.xls").getFile());
-		indPrueba = new Indicador("i_indprueba","c_PasivosTotales/(c_ActivosTotales-c_PasivosTotales)");
+		indPrueba = new Indicador("i_indprueba", "c_PasivosTotales/(c_ActivosTotales-c_PasivosTotales)");
 		t.agregarIndicador(indPrueba);
-		metodologiasPersistidos = Metodologias.setMetodologias();
 		periodo = new Periodo(LocalDate.of(2010, 04, 21), LocalDate.of(2017, 07, 21));
 
 		sum = new Sumatoria(indPrueba, t);
@@ -90,121 +85,61 @@ public class MetodologiaTest {
 		metod.agregarCondicion(condicion1);
 		metod.agregarCondicion(condicion2);
 
-//		try {
-//			
-//			EntityManager em = Utilidades.getEntityManager();
-//			EntityTransaction et = em.getTransaction();
-//			et.begin();
-//			em.persist(indPrueba);
-//			em.persist(sum);
-//			em.persist(condicion1);
-//			em.persist(condicion2);
-//			em.persist(metod);
-//			et.commit();
-//			metodologiasPersistidos.add(metod);
-//		} catch (PersistenceException e) {
-//			log.fatal("La metodologia ya esta cargada");
-//		}
-//		Utilidades.closeEntityManager();
+		EntityManager session = Utilidades.getEntityManager();
+		EntityTransaction et = session.getTransaction();
+		try {
+
+			et.begin();
+			session.persist(indPrueba);
+			session.persist(sum);
+			session.persist(condicion1);
+			session.persist(condicion2);
+			session.persist(metod);
+			et.commit();
+
+		} catch (PersistenceException e) {
+			log.fatal("La metodologia ya fue creada");
+		} finally {
+			if (session != null) {
+				Utilidades.closeEntityManager();
+			}
+		}
 
 	}
+	
+	
 
+	
 
 	@Test
 	public void aplicarMetodologiaEnBD()
 			throws NoHayEmpresasQueCumplanLaCondicionException, NoSeEncuentraLaEmpresaException, ScriptException,
 			NoSePudoOrdenarLaCondicionException, NoSeEncuentraLaCuentaException,
 			NoSeEncuentraLaCuentaEnElPeriodoException, NoSeEncuentraElIndicadorException {
-		
-//		Utilidades.closeEntityManager();		
+
+		List<Metodologia> metodologiasBD = Metodologias.setMetodologias();
+
+		Metodologia metodologia = metodologiasBD.stream().filter(unaM -> unaM.equals(metod)).findFirst().get();
+
 		EntityManager session = Utilidades.getEntityManager();
-		EntityTransaction et = session.getTransaction();
-		try {
-			et.begin();
-			session.persist(indPrueba);
-			session.persist(sum);
-			session.persist(condicion1);
-			session.persist(condicion2);
-			session.persist(metod);
-			et.commit();
-			Metodologia metodologia = session.find(Metodologia.class, metod.getId());
-
-			metodologia.getCondicionesDeMetodologia().stream().forEach(unC -> {
-				unC.getLadoIzq().setTraductor(t);
-			});
-			
-			ArrayList<PuntajeEmpresa> empresas = metodologia.aplicarMetodologia(t.getEmpresas());
-//			Metodologia metoAplicar = t.buscarMetodologia("testMetodotest");
-//
-//			ArrayList<PuntajeEmpresa> empresas = metoAplicar.aplicarMetodologia(t.getEmpresas());
-			// De un test anterior conozco los resultados
-			assertEquals(t.buscarEmpresaEnPuntajeEmpresa(empresas, "Facebook"), empresas.get(1));
-			assertEquals(t.buscarEmpresaEnPuntajeEmpresa(empresas, "Pepsico"), empresas.get(0));
-			
-			et.begin();
-
-			session.remove(indPrueba);
-			
-			session.remove(metodologia);
-
-			et.commit();
-
-		} catch (PersistenceException e) {
-			if (et != null) {
-				et.rollback();
-			}
-			throw new PersistenceException("No se pudo actualizar el objeto" + e.getMessage());
-		} finally {
-			if (session != null) { 
-				Utilidades.closeEntityManager();
-			}
-		}
 		
+
+		Metodologia meto = session.find(Metodologia.class, metodologia.getId());
+	
+		Utilidades.closeEntityManager();
+		meto.getCondicionesDeMetodologia().stream().forEach(unC -> {
+			unC.getLadoIzq().setTraductor(t);
+		});
+
+		ArrayList<PuntajeEmpresa> empresas = meto.aplicarMetodologia(t.getEmpresas());
+
+		// De un test anterior conozco los resultados
+		assertEquals(t.buscarEmpresaEnPuntajeEmpresa(empresas, "Facebook"), empresas.get(1));
+		assertEquals(t.buscarEmpresaEnPuntajeEmpresa(empresas, "Pepsico"), empresas.get(0));
 
 	}
 	
 	
-	@Test
-	public void eliminarMetodologia() {
-		
-//		Utilidades.closeEntityManager();		
-		EntityManager session = Utilidades.getEntityManager();
-		EntityTransaction et = session.getTransaction();
-		try {
-			et.begin();
-			session.persist(indPrueba);
-			session.persist(sum);
-			session.persist(condicion1);
-			session.persist(condicion2);
-			session.persist(metod);
-			et.commit();
-			Metodologia metodologia = session.find(Metodologia.class, metod.getId());
-
-			et.begin();
-
-			session.remove(indPrueba);
-			
-			session.remove(metodologia);
-
-			et.commit();
-			
-			Metodologia metodologiaenBD = session.find(Metodologia.class, metod.getId());
-			
-			assertTrue(metodologiaenBD == null);
-
-		} catch (PersistenceException e) {
-			if (et != null) {
-				et.rollback();
-			}
-			throw new PersistenceException("No se pudo actualizar el objeto" + e.getMessage());
-		} finally {
-			if (session != null) { 
-				Utilidades.closeEntityManager();
-			}
-		}
-		
-
-	}
 
 
 	@After
