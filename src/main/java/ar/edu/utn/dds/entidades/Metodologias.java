84410c891467f04250ab6ab44ba38852ce44b444
Nico <nico_nobile@hotmail.com>
2017-10-19 15:41:08 -0300

    Cambios para la persistencia en web

--- src/main/java/ar/edu/utn/dds/entidades/Metodologias.java
+++ src/main/java/ar/edu/utn/dds/entidades/Metodologias.java
@@ -2,13 +2,18 @@ package ar.edu.utn.dds.entidades;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceException;
 
 import ar.edu.utn.dds.modelo.Condicion;
+import ar.edu.utn.dds.modelo.FiltroSegunEcuacion;
 import ar.edu.utn.dds.modelo.Metodologia;
+import ar.edu.utn.dds.modelo.OrdenaAplicandoCriterioOrdenamiento;
+import ar.edu.utn.dds.modelo.Periodo;
+import ar.edu.utn.dds.modelo.ValorCalculable;
 import ar.edu.utn.dds.persistencia.Utilidades;
 
 public class Metodologias {
@@ -61,6 +66,41 @@ public class Metodologias {
 				Utilidades.closeEntityManager();
 			}
 		}
+
+	}
+	public static void persistirCrecienteoDecrecienteoLongevidad(Metodologia meto,Object tipo, Condicion cond) {
+		
+		List<Condicion> condiciones=new ArrayList<>();
+		condiciones.add(cond);
+		Metodologias.persistirCondicionesMetodologia(meto, tipo, condiciones);
+	}
+	
+	
+	
+	
+	public static void persistirMedianaOsumatoriaOPromedio(Metodologia meto,ValorCalculable valorCalculable, String idValor,
+			String idComparador, Periodo periodo, String idCriterio) {
+		Periodo p = new Periodo();
+		try {
+
+			p = Periodos.getPeriodos().stream().filter(unP -> unP.equals(periodo)).findFirst().get();
+		} catch (NoSuchElementException e) {
+			p = periodo;
+			Utilidades.persistirUnObjeto(p);
+			Periodos.agregarPeriodo(p);
+
+		}
+		Condicion condicion1 = new FiltroSegunEcuacion(valorCalculable, Integer.parseInt(idValor), idComparador, p);
+		Condicion condicion2 = new OrdenaAplicandoCriterioOrdenamiento(valorCalculable, p, idCriterio);
+		meto.agregarCondicion(condicion1);
+		meto.agregarCondicion(condicion2);
+
+	
+		List<Condicion> lista=new ArrayList<>();
+		
+		lista.add(condicion1);
+		lista.add(condicion2);
+		Metodologias.persistirCondicionesMetodologia(meto, valorCalculable,lista);
 
 	}
 }
